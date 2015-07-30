a8930d446ea993f7ad91e6c053767e43dfeea67c
nakagawa <nakagawa@localhost.localdomain>
2015-07-30 11:07:26 +0900

    Push Cake Framework

---
+++ app/Controller/AppController.php
@@ -0,0 +1,78 @@
+<?php
+/**
+ * Application level Controller
+ *
+ * This file is application-wide controller file. You can put all
+ * application-wide controller-related methods here.
+ *
+ * CakePHP(tm) : Rapid Development Framework (http://cakephp.org)
+ * Copyright (c) Cake Software Foundation, Inc. (http://cakefoundation.org)
+ *
+ * Licensed under The MIT License
+ * For full copyright and license information, please see the LICENSE.txt
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @copyright     Copyright (c) Cake Software Foundation, Inc. (http://cakefoundation.org)
+ * @link          http://cakephp.org CakePHP(tm) Project
+ * @package       app.Controller
+ * @since         CakePHP(tm) v 0.2.9
+ * @license       http://www.opensource.org/licenses/mit-license.php MIT License
+ */
+
+App::uses('Controller', 'Controller');
+
+/**
+ * Application Controller
+ *
+ * Add your application-wide methods in the class below, your controllers
+ * will inherit them.
+ *
+ * @package		app.Controller
+ * @link		http://book.cakephp.org/2.0/en/controllers.html#the-app-controller
+ */
+class AppController extends Controller {
+
+    protected function get_posted_params($key,$default=NULL)
+    {
+        // GET値取得
+        if(isset($this->request->query[$key]))
+        {
+            if(is_string($this->request->query[$key]) && $this->request->query[$key]==="")
+            {
+                return $default;
+            }
+
+            return $this->request->query[$key];
+        }
+
+        // POST値取得
+        if(isset($this->request->data[$key]))
+        {
+            if(is_string($this->request->data[$key]) && $this->request->data[$key]==="")
+            {
+                return $default;
+            }
+
+            return $this->request->data[$key];
+        }
+
+        return $default;
+    }
+
+    // 実行モード取得
+    public function is_execute_mote()
+    {
+        if(isset($this->request->query['exec']))
+        {
+            return $this->request->query['exec']==1;
+        }
+
+        // POST値取得
+        if(isset($this->request->data['exec']))
+        {
+            return $this->request->data['exec']==1;
+        }
+
+        return NULL;
+    }
+}
